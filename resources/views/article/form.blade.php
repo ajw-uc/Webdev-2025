f8713febdd94bf55ac979de1900c142d83c0d967
Andy <andy.wicaksono@ciputra.ac.id>
2026-05-10 21:38:21 +0700

    blade components

--- resources/views/article/form.blade.php
+++ resources/views/article/form.blade.php
@@ -1,29 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Artikel</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-</head>
-<body>
+<x-template title="Buat Artikel">
     <div class="container">
         <form method="post" class="was-validated">
             @csrf
-            <div class="mb-3">
-                <label for="title" class="form-label">Judul</label>
+            <x-form.group for="title" label="Judul">
                 <input type="text" name="title" id="title" class="form-control" required>
-            </div>
-            <div class="mb-3">
-                <label for="content" class="form-label">Isi</label>
+            </x-form.group>
+            <x-form.group for="content" label="Isi">
                 <textarea name="content" id="content" class="form-control" required></textarea>
-            </div>
+            </x-form.group>
             <div class="mb-3">
                 <button type="submit" class="btn btn-primary">Simpan</button>
             </div>
         </form>
     </div>
-</body>
-</html>
+</x-template>
--- resources/views/article/list.blade.php
+++ resources/views/article/list.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Artikel</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-</head>
-<body>
+<x-template title="Artikel">
     <div class="container">
         @if (count($articles) < 10)
             <a class="btn btn-success" href="{{ route('article.create') }}">Tambah Artikel</a>
@@ -25,5 +15,4 @@
             </div>
         @endforeach
     </div>
-</body>
-</html>
+</x-template>
