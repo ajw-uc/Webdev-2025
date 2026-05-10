80d06f4bce9388e5c22b2662ec434bdc72bc8ebf
Andy <andy.wicaksono@ciputra.ac.id>
2026-05-10 21:30:30 +0700

    bootstrap css

--- resources/views/article/form.blade.php
+++ resources/views/article/form.blade.php
@@ -4,20 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Artikel</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
 <body>
-    <form method="post">
-        <input type="hidden" name="_token" value="<?= csrf_token() ?>">
-
-        <div>
-            <label for="title">Judul</label>
-            <input type="text" name="title" id="title">
-        </div>
-        <div>
-            <label for="content">Isi</label>
-            <textarea name="content" id="content"></textarea>
-        </div>
-        <button type="submit">Simpan</button>
-    </form>
+    <div class="container">
+        <form method="post" class="was-validated">
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">Judul</label>
+                <input type="text" name="title" id="title" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="content" class="form-label">Isi</label>
+                <textarea name="content" id="content" class="form-control" required></textarea>
+            </div>
+            <div class="mb-3">
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
--- resources/views/article/list.blade.php
+++ resources/views/article/list.blade.php
@@ -4,21 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Artikel</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
 <body>
-    @if(count($articles) > 0)
-    <a href="{{ route('article.create') }}">Tambah artikel</a>
-    @endif
-
-    @foreach ($articles as $article)
-    <div>
-        <h4>{{ $article['title'] }}</h4>
-        {!! $article['content'] !!}
-        <div>
-            <small>{{ $article['date'] }}</small>
-        </div>
+    <div class="container">
+        @if (count($articles) < 10)
+            <a class="btn btn-success" href="{{ route('article.create') }}">Tambah Artikel</a>
+        @endif
+        @foreach($articles as $article)
+            <div class="card mt-3">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $article['title'] }}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">{{ $article['date'] }}</h6>
+                    <p class="card-text">
+                        {{ $article['content'] }}
+                    </p>
+                </div>
+            </div>
+        @endforeach
     </div>
-    <hr/>
-    @endforeach
 </body>
 </html>
